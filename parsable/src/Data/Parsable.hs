9522233790103c5ae1a5ddbfd1db5dfd66cfd31a
hololeap <hololeap@users.noreply.github.com>
2022-07-01 11:40:25 -0600

    parsable more or less working
    Signed-off-by: hololeap <hololeap@users.noreply.github.com>

--- parsable/src/Data/Parsable.hs
+++ parsable/src/Data/Parsable.hs
@@ -7,203 +7,189 @@ There is an implicit "soft isomorphism" between 'parser' and 'toString'.
 (Successfully parsing a string and then running 'toString' on the result
 should result in the original string.)
 
-'Parsable' instances must return a 'PartialParse' wrapping the data. This can
-be done easily with the 'endPartial' function.
+Data returned from 'parser' must be wrapped inside 'ParseResult'. This can
+be done easily with the 'checkCoverage' function.
 
 For instance:
 
-> parser = endPartial $ count 5 anyChar
+> parser = checkCoverage $ count 5 anyChar
+
+Note that 'runParseResult' will throw an error if no 'ParseCoverage' has
+been written (e.g. only functions like 'pure', 'lift', etc. have been called).
 
 === Language extensions
 
-Because 'parser' and 'parser'' do not take any arguments, it may be necessary
+Because 'parser' does not take any arguments, it may be necessary
 to explicitly declare the type of @t@ for these functions.
 
 It may be helpful to enable and use the @TypeApplications@ and possibly
-@ScopedTypeVariables@ extensions. This extends to helper functions that use
-'parser' and 'parser'', such as 'runParsable' and 'runParsable''.
+@ScopedTypeVariables@ extensions..
 
 Look at the @Language Extensions@ section of the GHC documentation for
 instructions on how to use these extensions.
 -}
 
-{-# Language ConstrainedClassMethods #-}
 {-# Language DefaultSignatures #-}
+{-# Language DeriveDataTypeable #-}
 {-# Language DeriveGeneric #-}
 {-# Language DeriveTraversable #-}
 {-# Language DerivingVia #-}
 {-# Language FlexibleContexts #-}
 {-# Language FlexibleInstances #-}
+{-# Language FunctionalDependencies #-}
 {-# Language GeneralizedNewtypeDeriving #-}
-{-# Language LambdaCase #-}
+{-# Language MultiParamTypeClasses #-}
+{-# Language OverloadedStrings #-}
+{-# Language QuantifiedConstraints #-}
 {-# Language ScopedTypeVariables #-}
+{-# Language TupleSections #-}
 {-# Language TypeApplications #-}
 {-# Language TypeFamilies #-}
-
--- Needed to remove the warning when we set the default type instance
--- for ParsableInput to ()
-{-# Options_GHC -Wno-unused-type-patterns #-}
 
 module Data.Parsable
     (
     -- * Parsing
       Parsable(..)
-    , parserValue
-    , parserValue'
+    , ParserName(..)
+    , runParsableT
     , runParsable
-    , runParsable'
+    -- ** Wrappers
     , NaturalParsable(..)
-    -- ** Partial parses
-    , PartialParse(..)
-    , getPartialParse
-    , isCompleteParse
-    , partialParses
-    -- ** Parsing functions
+    -- * Partial parses
+    , ParseCoverage(..)
+    -- ** As a Monad
+    , ParseResult
+    , runParseResult
+    -- * Parsing functions
     , satisfyAny
     , someAllowed
     , wordsWithSep
     , wordsWithSep'
-    , endPartial
+    , checkCoverage
+    , readParsec
     -- * Printing
     , Printable(..)
-    , ShowPrintable(..)
     , toText
+    -- ** Wrappers
+    , ShowPrintable(..)
     -- * Re-exports
+    , module Control.Monad
+    , module Control.Monad.Trans.Class
     , module Data.Char
-    , module Data.Functor.Identity
-    , module Data.Kind
     , module Data.String
-    , module Text.Parsec
-    , module Control.Applicative
-    , module Data.Functor.Slim.Apply
+    , module Text.Megaparsec
+    , module Text.Megaparsec.Char
     ) where
 
-import Control.Applicative (some)
+import Control.Applicative hiding (some, many)
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Writer.Strict (WriterT(..))
 import Data.Char
-import Data.Functor.Slim.Apply
+import Data.Data
 import Data.Functor.Identity
 import Data.Kind
-import Data.List.NonEmpty (NonEmpty)
-import Data.Semigroup.Slim.Traversable
+import Data.Maybe (fromMaybe)
+import Data.Semigroup (Last(..))
 import Data.String
 import Data.Text (Text, unpack)
 import GHC.Generics
-import Text.Parsec
--- import Text.Parsec.Token (GenTokenParser(natural))
--- import Text.Parsec.Language (haskell)
-
-
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Read (readMaybe)
 
 -- | If a parse succeeds for the beginning of the input, but then fails, we
---   wrap the successful part in the v'PartialParse' constructor. If the entire
---   parse was successful, we wrap it in 'CompleteParse'.
+--   choose the 'PartialParse' constructor. If the entire parse was successful,
+--   we choose 'CompleteParse'.
 --
---   This is mostly useful for testing parsers where we need to know if the
---   entire test string was parsed or not, but still allows for combining
---   individual parsers into larger ones.
-data PartialParse t
-    = PartialParse
-        String -- ^ The unparsed remainder. Useful for error messages
-        t
-    | CompleteParse t
-    deriving (Show, Eq, Ord, Functor, Foldable, Traversable, Generic, Generic1)
-    deriving Semigroup via (Ap' PartialParse t)
+--   This is mostly useful for testing parsers where we need to test them
+--   individually (must end with 'CompleteParse') and also composed together
+--   (individual parsers may end with 'PartialParse' if they do not conclude
+--   the larger parse).
+data ParseCoverage
+    = PartialParse String
+    | CompleteParse
+    deriving stock (Show, Eq, Ord)
+    deriving Semigroup via Last ParseCoverage
 
---   The general idea is that if many parses end with a v'CompleteParse', the
---   entire sequence of parses is itself a v'CompleteParse'. The opposite holds
---   for v'PartialParse' as well.
-instance Apply PartialParse where
-    CompleteParse  f <.> CompleteParse  x = CompleteParse  (f x)
-    PartialParse _ f <.> CompleteParse  x = CompleteParse  (f x)
-    CompleteParse  f <.> PartialParse s x = PartialParse s (f x)
-    PartialParse _ f <.> PartialParse s x = PartialParse s (f x)
+-- | Derives most instances via
+--   @('WriterT' ('Maybe' 'ParseCoverage') m a)@
+newtype ParseResult e s m a = ParseResult
+    { unwrapParseResult :: m (a, Maybe ParseCoverage) }
+    deriving stock Functor
+    deriving (Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadParsec e s)
+        via WriterT (Maybe ParseCoverage) m
+    deriving MonadTrans via WriterT (Maybe ParseCoverage)
 
--- | Extract the data from a 'PartialParse'
-getPartialParse :: PartialParse t -> t
-getPartialParse = \case
-    PartialParse _ t -> t
-    CompleteParse t -> t
+-- | Extract the result of a 'ParseResult' computation.
+--
+--   This will throw an error if no 'ParseCoverage' has been written
+--   (e.g. only functions like 'pure', 'lift', etc. have been called).
+runParseResult :: MonadFail m
+    => ParseResult e s m a
+    -> m (ParseCoverage, a)
+runParseResult = go <=< unwrapParseResult
+  where
+    go (x, Just c ) = pure (c, x)
+    go (_, Nothing) = fail "runParseResult was called, but no ParseCoverage was created!"
+{-# Inline runParseResult #-}
 
--- | Returns @True@ on 'CompleteParse' and @False@ on v'PartialParse'
-isCompleteParse :: PartialParse t -> Bool
-isCompleteParse = \case
-    PartialParse _ _ -> False
-    CompleteParse _ -> True
-
--- | Condense a non-empty list of 'PartialParse's to a single 'PartialParse'
---   carrying a non-empty list. Equivalent to 'sequence1'.
-partialParses :: NonEmpty (PartialParse a) -> PartialParse (NonEmpty a)
-partialParses = sequence1
+newtype ParserName a e s (m :: Type -> Type) = ParserName { getParserName :: String }
+    deriving stock (Show, Eq, Ord)
+    deriving newtype IsString
 
 -- | Represents types that have a valid Parsec parser.
-class Parsable t where
-    -- | Represents some other input that is needed to create the parser. This
-    --   defaults to @()@ and can safely be ignored in most cases, but is
-    --   available if needed.
-    type family ParsableInput t :: Type
-    type instance ParsableInput t = ()
-
-    parser :: (Stream s Identity Char, ParsableInput t ~ ())
-        => Parsec s () (PartialParse t)
-    parser = parser' ()
-
-    -- | A more general case of 'parser' for when @ParsableInput t@ is /not/ equal
-    --   to @()@. This must be defined instead of 'parser' if this is the case.
-    parser' :: Stream s Identity Char
-        => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
-        -> Parsec s () (PartialParse t)
-
-    default parser' :: (Stream s Identity Char, ParsableInput t ~ ())
-        => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
-        -> Parsec s () (PartialParse t)
-    parser' _ = parser
-
-    {-# Minimal parser | parser' #-}
-
--- | Extract the 'PartialParse' value when running 'parser'. This can be
---   convenient when it doesn't matter if it was a v'PartialParse' or a
---   v'CompleteParse'.
-parserValue :: (Parsable t, ParsableInput t ~ (), Stream s Identity Char)
-    => Parsec s () t
-parserValue = getPartialParse <$> parser
-
--- | The same as 'parserValue', but takes 'ParsableInput'.
-parserValue' :: (Parsable t, Stream s Identity Char)
-    => ParsableInput t -> Parsec s () t
-parserValue' = fmap getPartialParse . parser'
-
--- | Convenience method to run a 'Parsable' parser.
-runParsable :: forall t s. (Parsable t, Stream s Identity Char, ParsableInput t ~ ())
-    => SourceName -- ^ Only used in error messages and may be the empty string
-    -> s
-    -> Either ParseError (PartialParse t)
-runParsable = parse (parser @t)
-
--- | Same as 'runParsable' but takes a non-trivial 'ParsableInput' as an argument.
-runParsable' :: forall t s. (Parsable t, Stream s Identity Char)
-    => ParsableInput t -- ^ Input needed to build the 'Parsable' parser
-    -> SourceName      -- ^ Only used in error messages and may be the empty string
-    -> s
-    -> Either ParseError (PartialParse t)
-runParsable' i = parse (parser' @t i)
+class MonadParsec e s m => Parsable a (m :: Type -> Type) s e where
+    parser :: ParseResult e s m a
+    parserName :: ParserName a e s m
+    {-# Minimal parser, parserName #-}
 
 newtype NaturalParsable a = NaturalParsable
     { unwrapNaturalParsable :: a }
-    deriving (Read, Show, Eq, Ord, Num)
 
-instance Read a => Parsable (NaturalParsable a) where
-    parser = (<?> "natural number") $ endPartial $ do
-        x <- some digit
-        pure $ read x
+instance (MonadParsec e s m, MonadFail m, Token s ~ Char, Read a, Integral a, Typeable a)
+    => Parsable (NaturalParsable a) m s e where
+    parserName = "natural number"
+    parser = checkCoverage $ (<?> "natural number") $ do
+        ds <- some (satisfy isDigit)
+        case readMaybe ds of
+            Just i -> pure $ NaturalParsable i
+            Nothing -> fail $ "Could not parse as " ++ t ++ ": " ++ show ds
+      where
+        t = show $ typeRep $ Proxy @a
+
+-- | Convenience function to run a 'Parsable' parser.
+runParsableT :: forall a m s e. (Parsable a (ParsecT e s m) s e, Monad m)
+    => String -> s -> m (Either (ParseErrorBundle s e) (ParseCoverage, a))
+runParsableT = runParserT $ runParseResult (parser <?> n)
+    where n = getParserName $ parserName @a @(ParsecT e s m)
+
+runParsable :: forall a e s. Parsable a (ParsecT e s Identity) s e
+    => String -> s -> Either (ParseErrorBundle s e) (ParseCoverage, a)
+runParsable = runParser $ runParseResult (parser <?> n)
+    where n = getParserName $ parserName @a @(ParsecT e s Identity)
+
+-- | Pass a previously-parsed string to this function in order to attempt
+--   using 'read'. Produces proper error messages on failure.
+readParsec :: forall a e s m. (MonadParsec e s m, MonadFail m, Typeable a, Read a) => String -> m a
+readParsec s = (<?> typeName ++ " (Read instance)") $
+    case readMaybe s of
+        Just x -> pure x
+        Nothing -> fail
+            $  "unable to parse using "
+            ++ typeName
+            ++ " Read instance: "
+            ++ show s
+    where typeName = show $ typeRep $ Proxy @a
 
 -- | Parse a Char that satisfies any of the given predicates
-satisfyAny :: Stream s Identity Char => [Char -> Bool] -> Parsec s u Char
+satisfyAny :: MonadParsec e s m => [Token s -> Bool] -> m (Token s)
 satisfyAny fs = satisfy $ \c -> or [f c | f <- fs]
 
 -- | One or more characters that satisfy any of the given predicates
-someAllowed :: Stream s Identity Char
+someAllowed :: (MonadParsec e s m, Token s ~ Char)
     => [Char -> Bool]
-    -> Parsec s u (PartialParse String)
+    -> ParseResult e s m String
 someAllowed allowed = wordsWithSep allowed allowed []
 
 -- | Best effort parsing of "words" starting with certain allowed characters,
@@ -227,63 +213,63 @@ someAllowed allowed = wordsWithSep allowed allowed []
 --
 --  If a @wordsWithSep@ parser fails, it will return v'PartialParse' wrapping
 --  the string up to and including the last successfully parsed word.
-wordsWithSep :: Stream s Identity Char
+wordsWithSep :: (MonadParsec e s m, Token s ~ Char)
     => [Char -> Bool]    -- ^ Characters allowed at the start of a word
     -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
     -> [Char -> Bool]    -- ^ Characters that separate words
-    -> Parsec s u (PartialParse String)
+    -> ParseResult e s m String
 wordsWithSep = wordsWithSepG Nothing
 
 -- | The same as 'wordsWithSep', but this takes a parser which will be run in
 --   place of normal word detection for the first word. This allows for
 --   starting with generally unallowed characters before the first separator.
-wordsWithSep' :: Stream s Identity Char
-    => Parsec s u String -- ^ Beginning parser, skips normal beginning
+wordsWithSep' :: (MonadParsec e s m, Token s ~ Char)
+    => m String -- ^ Beginning of parser, skips normal beginning
     -> [Char -> Bool]    -- ^ Characters allowed at the start of a word
     -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
     -> [Char -> Bool]    -- ^ Characters that separate words
-    -> Parsec s u (PartialParse String)
+    -> ParseResult e s m String
 wordsWithSep' = wordsWithSepG . Just
 
 -- | General version of 'wordsWithSep'. Not exported.
-wordsWithSepG :: Stream s Identity Char
+wordsWithSepG :: (MonadParsec e s m, Token s ~ Char)
     -- | Optional beginning of parser, skips normal beginning
-    => Maybe (Parsec s u String)
+    => Maybe (m String)
     -> [Char -> Bool]    -- ^ Characters allowed at the start of a word
     -> [Char -> Bool]    -- ^ Characters allowed after the start of a word
     -> [Char -> Bool]    -- ^ Characters that separate words
-    -> Parsec s u (PartialParse String)
+    -> ParseResult e s m String
 wordsWithSepG maybeBeg wordStart wordRest wordSep = do
-    beg <- case maybeBeg of
-        Just p -> p
-        Nothing -> do
-            w <- some $ satisfyAny wordStart
-            r <- many $ satisfyAny wordRest
-            pure $ w ++ r
+    let beg = flip fromMaybe maybeBeg $ do
+                    w <- some $ satisfyAny wordStart
+                    r <- many $ satisfyAny wordRest
+                    pure $ w ++ r
     choice
         [ try $ do
+            b    <- lift beg
             sep  <- satisfyAny wordSep
             next <- wordsWithSepG Nothing wordStart wordRest wordSep
-            pure $ ((beg ++ [sep]) ++) <$> next
-        , endPartial $ pure beg
+            pure $ b ++ [sep] ++ next
+        , checkCoverage beg
         ]
 
--- | Run the specified parser, then return 'CompleteParse' if we are at
---   'eof', otherwise 'PartialParse'.
-endPartial :: Stream s Identity Char
-    => Parsec s u t
-    -> Parsec s u (PartialParse t)
-endPartial p = do
-    t <- p
-    f <- choice
-            [ CompleteParse <$ eof
-            , PartialParse <$> lookAhead (some anyChar)
-            ]
-    pure $ f t
+-- | Run the specified parser, then write 'CompleteParse' if we are at
+--   'eof', otherwise v'PartialParse'.
+checkCoverage :: (MonadParsec e s m, Token s ~ Char)
+    => m a
+    -> ParseResult e s m a
+checkCoverage p = ParseResult $ p >>= \x ->
+    (x,) . Just <$> choice
+        [ CompleteParse <$ eof
+        , PartialParse <$> lookAhead (some anySingle)
+        ]
 
 -- | Types that can be converted back to a @String@.
 class Printable t where
     toString :: t -> String
+
+    default toString :: Show t => t -> String
+    toString = show
     {-# Minimal toString #-}
 
 instance Printable String where
@@ -292,16 +278,27 @@ instance Printable String where
 instance Printable Text where
     toString = unpack
 
--- | Convenience method that will turn a 'Printable' to any 'IsString'.
+-- | Convenience function that will turn a 'Printable' to any 'IsString'.
 toText :: (Printable t, IsString s) => t -> s
 toText = fromString . toString
 
 -- | Wrapper for types that inherit 'toString' directly from their 'Show' instance.
 --
 --   It is convenient to use the @DerivingVia@ language extension with this.
-newtype ShowPrintable a = ShowPrintable {
-    unwrapShowPrintable :: a
-} deriving (Read, Show, Eq, Ord, Num, IsString)
+--
+--   > {-# Language DerivingVia #-}
+--   >
+--   > newtype MyNum Int
+--   >     deriving Printable via (ShowPrintable Int) -- Uses Show instance of Int
+newtype ShowPrintable a = ShowPrintable
+    { unwrapShowPrintable :: a }
+    deriving stock
+        ( Read, Show, Eq, Ord, Bounded, Functor, Foldable, Traversable, Generic
+        , Generic1, Data
+        )
+    deriving newtype (Enum, Num, Fractional, Floating, IsString, Semigroup, Monoid)
+    deriving (Applicative, Monad) via Identity
 
+-- | Uses 'show' after unwrapping the contents.
 instance Show a => Printable (ShowPrintable a) where
     toString = show . unwrapShowPrintable
--- parsable/test/Main.hs
+++ parsable/test/Main.hs
@@ -8,21 +8,27 @@ module Main (main) where
 
 import Data.Parsable
 import Data.Proxy
-import Test.QuickCheck
+import Test.Tasty
+import Test.Tasty.QuickCheck
 import Test.QuickCheck.Classes
 
-import qualified Data.Functor.Slim.Apply as Slim
-import qualified Data.Functor.Apply as Semigroupoids
-
-instance Semigroupoids.Apply PartialParse where
-    (<.>) = (Slim.<.>)
-
-instance Arbitrary a => Arbitrary (PartialParse a) where
-    arbitrary :: Gen (PartialParse a)
+instance Arbitrary a => Arbitrary (ParseResult a) where
+    arbitrary :: Gen (ParseResult a)
     arbitrary = do
+        x1 <- arbitrary
+        x2 <- arbitrary
         s <- listOf chooseAny
-        elements [CompleteParse, PartialParse s] <*> arbitrary
+        elements [CompleteParse x1, PartialParse s x2]
 
 main :: IO ()
-main = do
-    lawsCheck $ applyLaws $ Proxy @PartialParse
+main = defaultMain $ testGroup "parsable tests"
+    [ classesTest
+    ]
+
+classesTest :: TestTree
+classesTest = testProperty "quickcheck-classes" $ ioProperty $
+    mapM_ (\f -> lawsCheck $ f $ Proxy @ParseResult)
+        [ functorLaws
+        , foldableLaws
+        , traversableLaws
+        ]
