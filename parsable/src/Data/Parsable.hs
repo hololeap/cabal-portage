6491c1fdee0d43724ec56c592376abd12fe4c695
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-05-18 00:45:37 -0600

    porting Parsable to FlatParse
    Co-authored-by: hololeap <hololeap@protonmail.com>
    Signed-off-by: hololeap <hololeap@protonmail.com>

--- parsable/src/Data/Parsable.hs
+++ parsable/src/Data/Parsable.hs
@@ -19,6 +19,7 @@ Look at the @Language Extensions@ section of the GHC documentation for
 instructions on how to use these extensions.
 -}
 
+{-# Language DataKinds #-}
 {-# Language DefaultSignatures #-}
 {-# Language DeriveDataTypeable #-}
 {-# Language DeriveGeneric #-}
@@ -40,8 +41,12 @@ module Data.Parsable
     -- * Parsing
       Parsable(..)
     , ParserName(..)
-    , runParsableT
+    , runParsableIO
+    , runParsableST
     , runParsable
+    , extractResult
+    , (<?>)
+    , choice
     -- ** Wrappers
     , NaturalParsable(..)
     -- * Parsing functions
@@ -59,66 +64,92 @@ module Data.Parsable
     , module Data.Char
     , module Data.Functor.Identity
     , module Data.String
-    , module Text.Parsec
-    , module Text.Parsec.Char
+    , module FlatParse.Basic
+    , module FlatParse.Basic.Text
+    , IOMode
+    , PureMode
+    , STMode
     ) where
 
-import Control.Applicative hiding (many)
+import Control.Applicative hiding (many, (<|>))
 import Control.Monad
+import Control.Monad.ST (ST)
 import Control.Monad.Trans.Class
-import Data.Char
+import Data.ByteString (ByteString)
+import Data.Char hiding (isDigit)
 import Data.Data
 import Data.Functor.Identity
-import Data.Kind
 import Data.String
 import Data.Text (Text, unpack)
+import FlatParse.Basic
+import FlatParse.Basic.Text
+import FlatParse.Common.Parser (IOMode, PureMode, STMode)
+import GHC.Exts (ZeroBitType)
 import GHC.Generics
-import Text.Parsec
-import Text.Parsec.Char
 import Text.Read (readMaybe)
 
-newtype ParserName a s u (m :: Type -> Type) = ParserName { getParserName :: String }
+newtype ParserName a (st :: ZeroBitType) e = ParserName { getParserName :: String }
     deriving stock (Show, Eq, Ord)
     deriving newtype IsString
 
 -- | Represents types that have a valid Parsec parser.
-class Parsable a (m :: Type -> Type) u s where
-    parser :: ParsecT s u m a
-    parserName :: ParserName a s u m
+class Parsable a (st :: ZeroBitType) e where
+    parser :: ParserT st e a
+    parserName :: ParserName a st e
     {-# Minimal parser, parserName #-}
 
 newtype NaturalParsable a = NaturalParsable
     { unwrapNaturalParsable :: a }
 
-instance (Stream s m Char, Read a, Typeable a)
-    => Parsable (NaturalParsable a) m u s where
+instance (Read a, Typeable a)
+    => Parsable (NaturalParsable a) st String where
     parserName = "natural number"
     parser = (<?> "natural number") $ do
         ds <- some (satisfy isDigit)
         case readMaybe ds of
             Just i -> pure $ NaturalParsable i
-            Nothing -> fail $ "Could not parse as " ++ t ++ ": " ++ show ds
+            Nothing -> err $ "Could not parse as " ++ t ++ ": " ++ show ds
       where
         t = show $ typeRep $ Proxy @a
 
--- | Convenience function to run a 'Parsable' parser.
-runParsableT :: forall a m s. (Stream s m Char, Parsable a m () s)
-    => String -> s -> m (Either ParseError a)
-runParsableT = runParserT (parser <?> n) ()
-    where n = getParserName $ parserName @a @m @() @s
+infix 0 <?>
+(<?>) :: ParserT st String a -> String -> ParserT st String a
+(<?>) p e = withError p (\e' -> err (unlines [e,e']))
 
-runParsable :: forall a s. (Stream s Identity Char, Parsable a Identity () s)
-    => String -> s -> Either ParseError a
-runParsable = runParser (parser <?> n) ()
-    where n = getParserName $ parserName @a @Identity @() @s
+-- | Similar to 'asum', but adds 'try' to all choices except the last.
+choice :: [ParserT st e a] -> ParserT st e a
+choice [p] = p
+choice (p:ps) = try p <|> choice ps
+choice [] = empty
+
+-- | Convenience function to run a 'Parsable' parser.
+runParsableIO :: forall a. (Parsable a IOMode String)
+    => ByteString -> IO (Either (Maybe String) a)
+runParsableIO bs = extractResult <$> runParserIO (parser <?> n) bs
+    where n = getParserName $ parserName @a @IOMode @String
+
+runParsableST :: forall a s. (Parsable a (STMode s) String)
+    => ByteString -> ST s (Either (Maybe String) a)
+runParsableST bs = extractResult <$> runParserST (parser <?> n) bs
+    where n = getParserName $ parserName @a @(STMode s) @String
+
+runParsable :: forall a. (Parsable a PureMode String)
+    => ByteString -> Either (Maybe String) a
+runParsable = extractResult . runParser (parser <?> n)
+    where n = getParserName $ parserName @a @PureMode @String
+
+extractResult :: Result e a -> Either (Maybe e) a
+extractResult (OK a _) = Right a
+extractResult (Err e)  = Left (Just e)
+extractResult Fail     = Left Nothing
 
 -- | Pass a previously-parsed string to this function in order to attempt
 --   using 'read'. Produces proper error messages on failure.
-readParsec :: forall a s u m. (Typeable a, Read a) => String -> ParsecT s u m a
+readParsec :: forall a st. (Typeable a, Read a) => String -> ParserT st String a
 readParsec s = (<?> typeName ++ " (Read instance)") $
     case readMaybe s of
         Just x -> pure x
-        Nothing -> fail
+        Nothing -> err
             $  "unable to parse using "
             ++ typeName
             ++ " Read instance: "
@@ -126,16 +157,16 @@ readParsec s = (<?> typeName ++ " (Read instance)") $
     where typeName = show $ typeRep $ Proxy @a
 
 -- | Parse a token that satisfies any of the given predicates
-satisfyAny :: Stream s m Char => [Char -> Bool] -> ParsecT s u m Char
+satisfyAny :: [Char -> Bool] -> ParserT st e Char
 satisfyAny fs = satisfy $ \c -> or [f c | f <- fs]
 
 -- | Parsing of "words" which require a list of predicates for the first
 --   token, and a list of predicates for any remaining tokens. This always
 --   parses at least one token.
-wordAllowed :: Stream s m Char
-    => [Char -> Bool] -- ^ Tokens that start the word
+wordAllowed
+    :: [Char -> Bool] -- ^ Tokens that start the word
     -> [Char -> Bool] -- ^ Any subsequent tokens
-    -> ParsecT s u m [Char]
+    -> ParserT st e [Char]
 wordAllowed beg rest = (:) <$> satisfyAny beg <*> many (satisfyAny rest)
 
 -- | Types that can be converted back to a @String@.
