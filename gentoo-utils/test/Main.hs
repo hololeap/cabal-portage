eaaad0a03509b4f3a4f73b8f570e7a83ea16ef1a
hololeap <hololeap@protonmail.com>
2025-08-17 19:10:19 -0600

    gentoo-utils: Add really basic tests
    Signed-off-by: hololeap <hololeap@protonmail.com>

--- gentoo-utils/test/Main.hs
+++ gentoo-utils/test/Main.hs
@@ -1,4 +1,28 @@
 module Main (main) where
 
+import qualified Data.Text.Lazy as Lazy
+import System.Directory (findExecutable)
+
+import Distribution.Gentoo.Utils.Process
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = go $ \exe -> do
+    let args = ["it", "works!"]
+
+    putStrLn $ showCmd "runTransparent" exe args
+    (o,e) <- runTransparent exe args
+    putStrLn $ "stdout: " ++ show (Lazy.toChunks o)
+    putStrLn $ "stderr: " ++ show (Lazy.toChunks e)
+    putStrLn ""
+
+    putStrLn $ showCmd "runOpaque" exe args
+    (o',e') <- runOpaque exe args
+    putStrLn $ "stdout: " ++ show (Lazy.toChunks o')
+    putStrLn $ "stderr: " ++ show (Lazy.toChunks e')
+    putStrLn ""
+  where
+    go :: (FilePath -> IO ()) -> IO ()
+    go f = findExecutable "echo" >>= mapM_ f
+
+    showCmd :: String -> FilePath -> [String] -> String
+    showCmd func exe args = unwords $ [func, show exe, show args]
